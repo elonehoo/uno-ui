0cdf8bf998655ccfe9821ff4953d62c30332f7a3
xiaoxunyao <huchengyea@163.com>
2022-05-08 21:54:50 +0800

    chore: init

---
+++ packages/uno-ui/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    Unocss()
+  ],
+})
