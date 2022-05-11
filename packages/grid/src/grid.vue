453c86227e14cec67e6e5b2afcf8e9b0692fc549
xiaoxunyao <huchengyea@163.com>
2022-05-11 21:59:02 +0800

    chore: init `unoGrid`

---
+++ packages/grid/src/grid.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="grid grid-cols-3 gap-4">
+    <slot />
+  </div>
+</template>
---
+++ packages/grid/src/index.ts
@@ -0,0 +1,3 @@
+import unoGrid from './grid.vue'
+
+export {unoGrid}
